f2e3003269b4a0aa3d5617ea903e4a00c46fbc9c
shasho <sys.sharon2@gmail.com>
2019-04-14 14:53:39 +0300

    push

--- GetAppRecommendationSystem/Main.py
+++ GetAppRecommendationSystem/Main.py
@@ -1,3 +1,4 @@
 import ParseAndSort
 
 ParseAndSort.ML("Data/activityRanking.csv","Data/activity.csv")
+ParseAndSort.ML("Data/activityRanking2.csv","Data/activity2.csv")
--- GetAppRecommendationSystem/ParseAndSort.py
+++ GetAppRecommendationSystem/ParseAndSort.py
@@ -1,45 +1,48 @@
 import numpy as np  
 import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
 
 def ML(activityRankingCSV,activityCSV):
+    print("****ML***")
     ratings_data = pd.read_csv(activityRankingCSV)  
     ratings_data.head()  
     activity_names = pd.read_csv(activityCSV)  
     activity_names.head()  
-    print ("*********ratings_data: *********")
-    print(ratings_data)
-    print ("*********/ratings_data: *********")
-    print("\n")
-    print ("*********Activity Name: *********")
-    print(activity_names)
-    print ("*********/Activity Name: *********")
-    print("\n")
+#     print ("*********ratings_data: *********")
+#     print(ratings_data)
+#     print ("*********/ratings_data: *********")
+#     print("\n")
+#     print ("*********Activity Name: *********")
+#     print(activity_names)
+#     print ("*********/Activity Name: *********")
+#     print("\n")
 
     # activity_names['activity_id'] = ratings_data['activity_id'].astype(float)
 
     activity_data = pd.merge(ratings_data, activity_names, on='activity_id')  
 
-    print ("*********activity_data: *********")
-    print(activity_data)
-    print ("*********/activity_data: *********")
-    print("\n")
+#     print ("*********activity_data: *********")
+#     print(activity_data)
+#     print ("*********/activity_data: *********")
+#     print("\n")
 
     activity_data.head()  
     activity_data.groupby('name')['rank'].mean().head()  
     activity_data.groupby('name')['rank'].mean().sort_values(ascending=False).head()  
     activity_data.groupby('name')['rank'].count().sort_values(ascending=False).head()  
-    print ("*********activity_data2: *********")
-    print(activity_data)
-    print ("*********/activity_data2: *********")
-    print("\n")
+#     print ("*********activity_data2: *********")
+#     print(activity_data)
+#     print ("*********/activity_data2: *********")
+#     print("\n")
 
 
 
     ratings_mean_count = pd.DataFrame(activity_data.groupby('name')['rank'].mean())  
-    print ("*********post ratings_mean_count: *********")
-    print(ratings_mean_count)
-    print ("*********/ post ratings_mean_count: *********")
-    print("\n")
+#     print ("*********post ratings_mean_count: *********")
+#     print(ratings_mean_count)
+#     print ("*********/ post ratings_mean_count: *********")
+#     print("\n")
     ratings_mean_count['rating_counts'] = pd.DataFrame(activity_data.groupby('name')['rank'].count())
 
 
@@ -53,29 +56,41 @@ def ML(activityRankingCSV,activityCSV):
     # sns.jointplot(x='rank', y='rating_counts', data=ratings_mean_count, alpha=0.4) 
     # plt.show()
     # print(activity_data)
-    user_activity_rating = activity_data.pivot_table(index='user_id', columns='name', values='rank')  
-    # print(user_activity_rating)
+    user_activity_rating = activity_data.pivot_table(index='user_id', columns='name', values='rank')
+#     print ("*********user_activity_rating: *********")
+#     print(user_activity_rating)
+#     print ("*********/ user_activity_rating: *********")
+#     print("\n")
+  
+#     print ("*********user_activity_rating: *********")
+#     print(user_activity_rating)
+#     print ("*********/ user_activity_rating: *********")
+#     print("\n")
     user_activity_rating.head()  
-    football_rating = user_activity_rating['Football babyyyyy']  
+#     football_rating = user_activity_rating['Football babyyyyy'] 
+    football_rating = user_activity_rating['Tel aviv football club']  
+  
     football_rating.head()  
     activities_like_football = user_activity_rating.corrwith(football_rating)
-    corr_football = pd.DataFrame(activities_like_football, columns=['Correlation'])  
+    corr_football = pd.DataFrame(activities_like_football, columns=['Correlation']) 
+
+
     corr_football.dropna(inplace=True)  
     corr_football.head() 
     corr_football.sort_values('Correlation', ascending=False).head(10)      
     corr_football = corr_football.join(ratings_mean_count['rating_counts'])  
     corr_football.head()  
     corrResults = corr_football[corr_football ['rating_counts']>50].sort_values('Correlation', ascending=False)
-    print ("*********final corr results: *********")
-    print(corr_football)
-    print ("*********/ final corr results: *********")
-    print("\n")
+#     print ("*********final corr results: *********")
+#     print(corr_football)
+#     print ("*********/ final corr results: *********")
+#     print("\n")
 
     names = corr_football.index.get_level_values(0).values
-    print ("*********Activity Name: *********")
-    print(activity_names)
-    print ("*********/Activity Name: *********")
-    print("\n")
+#     print ("*********Activity Name: *********")
+#     print(activity_names)
+#     print ("*********/Activity Name: *********")
+#     print("\n")
 
     # print (please)
 
@@ -84,11 +99,10 @@ def ML(activityRankingCSV,activityCSV):
         
 
     for name in names:
-        print ("")
+        # print ("")
         rating = corr_football.loc[name,:]
         # print (rating)
-        print (name)
-        print (rating["Correlation"])
-        print (rating["rating_counts"])
-        print ("Your recommended sub categories is: "+str(activity_names.loc[name]["sub_id"]))
+        # print (name)
+        # print (rating["rating_counts"])
+        print ("Your recommended sub categories is: "+str(activity_names.loc[name]["sub_id"])+" Correlation: "+str(rating["Correlation"]))
 
